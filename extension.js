fa082bcc9a1128b89df6dbe1390e404760926e05
pilot22 <louis.champi44@gmail.com>
2020-07-11 12:23:36 +0200

    removed helloWorld command

--- extension.js
+++ extension.js
@@ -1,9 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-const {
-    cpuUsage
-} = require('process');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -46,6 +43,7 @@ function activate(context) {
             let result = ""
             
             chars.forEach(c => {
+                //implement if not isInt() continue
                 result += String.fromCharCode(c);
             })
 
